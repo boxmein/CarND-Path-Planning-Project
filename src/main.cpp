d542d4a2b4a1b4005a0a92171753dab19b185c3f
boxmein <johannes@qminderapp.com>
2017-09-26 00:09:25 +0300

    Add basic lane keeping and lane changing

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,41 @@
 #include "Eigen-3.3/Eigen/Core"
 #include "Eigen-3.3/Eigen/QR"
 #include "json.hpp"
+#include "spline.h"
 
 using namespace std;
 
 // for convenience
 using json = nlohmann::json;
+
+/** X/Y point */
+typedef struct xyPoint {
+  double x;
+  double y;
+} xyPoint;
+
+/** Frenet point */
+typedef struct frPoint {
+  double s;
+  double d;
+} frPoint;
+
+/** Car state */
+enum State {
+  KEEP_LANE,
+  PREP_LCH_LEFT,
+  PREP_LCH_RIGHT,
+  LCH_LEFT,
+  LCH_RIGHT,
+  STOP,
+  START
+};
+
+// The car's maximum allowed speed in mph
+constexpr double MAX_SPEED = 49.5;
+// The car's maximum allowed acceleration in m/s2
+constexpr double MAX_ACCEL = 5.0;
+
 
 // For converting back and forth between radians and degrees.
 constexpr double pi() { return M_PI; }
@@ -83,7 +113,7 @@ int NextWaypoint(double x, double y, double theta, const vector<double> &maps_x,
 }
 
 // Transform from Cartesian x,y coordinates to Frenet s,d coordinates
-vector<double> getFrenet(double x, double y, double theta, const vector<double> &maps_x, const vector<double> &maps_y)
+frPoint getFrenet(double x, double y, double theta, const vector<double> &maps_x, const vector<double> &maps_y)
 {
 	int next_wp = NextWaypoint(x,y, theta, maps_x,maps_y);
 
@@ -127,12 +157,40 @@ vector<double> getFrenet(double x, double y, double theta, const vector<double> 
 
 	frenet_s += distance(0,0,proj_x,proj_y);
 
-	return {frenet_s,frenet_d};
+	return {.s = frenet_s, .d = frenet_d};
 
 }
 
+
+// Get the D coordinate for a given lane.
+// The leftmost lane is 0
+// Waypoints follow the yellow lines
+// Each lane is 4 m wide, and to stay in the center, you want to add 2 m.
+// The starting lane is second from left (lane = 1)
+double getDForLane(int lane) {
+  if (lane < 0) {
+    cout << "Lane is in the opposite direction" << endl;
+  }
+  
+  return lane * 4 + 2;
+}
+
+int getLaneForD(double d) {
+  return round(d / 4.0) - 2;
+}
+
+// Transform world coordinates to car coordinates.
+xyPoint transformToLocal(double x, double y, double car_x, double car_y, double car_theta) {
+  xyPoint p;
+  double dx = x - car_x;
+  double dy = y - car_y;
+  p.x = dx * cos(-car_theta) - dy * sin(-car_theta);
+  p.y = dx * sin(-car_theta) + dy * cos(-car_theta);
+  return p;
+}
+
 // Transform from Frenet s,d coordinates to Cartesian x,y
-vector<double> getXY(double s, double d, const vector<double> &maps_s, const vector<double> &maps_x, const vector<double> &maps_y)
+xyPoint getXY(double s, double d, const vector<double> &maps_s, const vector<double> &maps_x, const vector<double> &maps_y)
 {
 	int prev_wp = -1;
 
@@ -152,15 +210,22 @@ vector<double> getXY(double s, double d, const vector<double> &maps_s, const vec
 
 	double perp_heading = heading-pi()/2;
 
-	double x = seg_x + d*cos(perp_heading);
-	double y = seg_y + d*sin(perp_heading);
+	double x = seg_x + d * cos(perp_heading);
+	double y = seg_y + d * sin(perp_heading);
 
-	return {x,y};
+	return { .x = x, .y = y };
 
 }
 
 int main() {
   uWS::Hub h;
+  
+  // Car state:
+  
+  // The car's target lane.
+  int target_lane = 1;
+  // The car's target speed. 50mph is the limit.
+  double target_speed = 0.;
 
   // Load up map values for waypoint's x,y,s and d normalized normal vectors
   vector<double> map_waypoints_x;
@@ -173,7 +238,7 @@ int main() {
   string map_file_ = "../data/highway_map.csv";
   // The max s value before wrapping around the track back to 0
   double max_s = 6945.554;
-
+  
   ifstream in_map_(map_file_.c_str(), ifstream::in);
 
   string line;
@@ -195,8 +260,8 @@ int main() {
   	map_waypoints_dx.push_back(d_x);
   	map_waypoints_dy.push_back(d_y);
   }
-
-  h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
+  
+  h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy,&target_lane,&target_speed](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                      uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
@@ -213,38 +278,226 @@ int main() {
         string event = j[0].get<string>();
         
         if (event == "telemetry") {
-          // j[1] is the data JSON object
-          
-        	// Main car's localization Data
+            // j[1] is the data JSON object
+        	  // Main car's localization Data
           	double car_x = j[1]["x"];
           	double car_y = j[1]["y"];
           	double car_s = j[1]["s"];
           	double car_d = j[1]["d"];
+            // Car D:
+            // 0 1 2 3 4 5 6 7 8 9 10 11 12
+            // =       |       |         |
           	double car_yaw = j[1]["yaw"];
           	double car_speed = j[1]["speed"];
+            
+            // True if we are too close to the particular vehicle.
+            bool too_close = false;
+            
+            // Telemetry!
+            cout << "T x=" << car_x << " y=" << car_y << " s=" << car_s << " d=" << car_d << " yaw=" << car_yaw
+                 << " speed=" << car_speed << endl;
+            
 
           	// Previous path data given to the Planner
           	auto previous_path_x = j[1]["previous_path_x"];
           	auto previous_path_y = j[1]["previous_path_y"];
-          	// Previous path's end s and d values 
+            
+          	// Previous path's end s and d values
           	double end_path_s = j[1]["end_path_s"];
           	double end_path_d = j[1]["end_path_d"];
-
+            
           	// Sensor Fusion Data, a list of all other cars on the same side of the road.
           	auto sensor_fusion = j[1]["sensor_fusion"];
+            
+            // int current_lane = getLaneForD(car_d);
 
           	json msgJson;
 
+            // X coordinates of path planner points
+            vector<double> ptsx;
+            
+            // Y coordinates of path planner points
+            vector<double> ptsy;
+
+            // X coordinate list sent to car
           	vector<double> next_x_vals;
+            
+            // Y coordinate list sent to car
           	vector<double> next_y_vals;
+            
+            xyPoint xy;
+                        
+            const int path_size = previous_path_x.size();
+            
+            double ref_x = car_x;
+            double ref_y = car_y;
+            double ref_yaw = deg2rad(car_yaw);
+            
+            // Simple case of KEEP LANE:
+            // Is there a car in front of us?
+            // Rather... Is there a car in front of our path?
+            
+            if (path_size > 0) {
+              car_s = end_path_s;
+            }
+            
+            for (int i = 0; i < sensor_fusion.size(); i++) {
+              vector<double> sf_car = sensor_fusion[i];
+              
+              // Car vector: [., ., ., vx, vy, s, d]
+              //              0  1  2   3   4  5  6
+              double d = sf_car[6];
+              
+              if (d < (2 + 4*target_lane + 2) && d > (2 + 4 * target_lane - 2)) {
+                    double s = sf_car[5];
+                    double vx = sf_car[3];
+                    double vy = sf_car[4];
+                    double v  = sqrt(vx * vx + vy * vy);
+                    
+                    // Extrapolate that the car will be moving linearly in frenet for as long as our path lasts.
+                    s += path_size * .02 * v;
+                    
+                    // If the car is in front of the path and if it's too close,
+                    // reduce the car's target velocity.
+                    if ((s > car_s) && ((s - car_s) < 30)) {
+                      cout << "Car in front! cars=" << car_s << " s=" << s << " card=" << car_d << "d=" << d << endl;
+                      too_close = true;
+                      
+                      if (target_lane != 0) {
+                        target_lane--;
+                      } else if (target_lane != 2) {
+                        target_lane++;
+                      }
+                    }
+              }
+            }
+            
+            if (too_close) {
+              target_speed -= .224;
+            } else if (target_speed < 49.5) {
+              target_speed += .224;
+            }
+            
+            
+            // Grab some historical data for the car.
+            // This will help in creating a better spline.
+            // xref_prev---xref---CAR--->x--->x--->
+            if (path_size < 2) {
+              ptsx.push_back(car_x - cos(car_yaw));
+              ptsy.push_back(car_y - sin(car_yaw));
+              
+              ptsx.push_back(car_x);
+              ptsy.push_back(car_y);
+            } else {
+              const double ref_x_prev = previous_path_x[path_size - 2];
+              const double ref_y_prev = previous_path_y[path_size - 2];
+              
+              ref_x = previous_path_x[path_size - 1];
+              ref_y = previous_path_y[path_size - 1];
+              
+              ref_yaw = atan2(
+                ref_y - ref_y_prev,
+                ref_x - ref_x_prev
+              );
+              
+              ptsx.push_back(ref_x_prev);
+              ptsy.push_back(ref_y_prev);
+              
+              ptsx.push_back(ref_x);
+              ptsy.push_back(ref_y);
+            }
 
-
-          	// TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
+            // Add 3 future points spaced 30m apart, for spline generation
+            for (int i = 1; i < 4; i++) {
+              xy = getXY(car_s + i * 30, getDForLane(target_lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+              ptsx.push_back(xy.x);
+              ptsy.push_back(xy.y);
+            }
+            
+            // Transform the entire current ptsx list to car-local coordinates
+            for (int i = 0; i < ptsx.size(); i++) {
+              /*xyPoint t = transformToLocal(ptsx[i], ptsy[i], ref_x, ref_y, ref_yaw);
+              ptsx[i] = t.x;
+              ptsy[i] = t.y; */
+              
+              double dx = ptsx[i] - ref_x;
+              double dy = ptsy[i] - ref_y;
+              
+              ptsx[i] = (dx * cos(- ref_yaw) - dy * sin(- ref_yaw));
+              ptsy[i] = (dx * sin(- ref_yaw) + dy * cos(- ref_yaw));
+            }
+            
+            //
+            // Smooth the resulting path via spline.h
+            //
+            
+            tk::spline s;
+            s.set_points(ptsx, ptsy);
+            
+            
+            //
+            // Generate the future path:
+            // - Add unused historical points onto the future path
+            // - Add enough future points to keep future path at 50 long
+            //
+            
+            for (int i = 0; i < path_size; i++) {
+              next_x_vals.push_back(previous_path_x[i]);
+              next_y_vals.push_back(previous_path_y[i]);
+            }
+            
+            /*
+             In car-local coordinates, figure out the x & y to follow in order to follow the spline.
+             
+             Space each xy point out by a specified distance to make sure that when a point is attained every 0.2s,
+             the speed traveled from point to point will be the target speed in m/s.
+             
+             
+             
+             y              + + + +
+             ^          + +
+             |      + +
+             |    +                 30
+             | +                  /
+             |-|-|-|-|-|-|-|-|-|-|-> x
+            */
+            
+            const double target_x = 30.0;
+            const double target_y = s(target_x);
+            const double target_dist = sqrt(target_x * target_x + target_y * target_y);
+            
+            // Keeps track of the X coordinate over the loop.
+            double x_addon = 0.;
+            
+            for (int i = 1; i <= (50 - path_size); i++) {
+              // Find the number of points to generate in order to get 30m forward:
+              // 30m / (200ms per iteration) * ()(target velocity in mph / 2.24) = (target vel in m/s)).
+              double N = (target_dist / (.02 * target_speed / 2.24));
+              
+              // The next point will be 30 meters / number of points to the right
+              double x_pt = x_addon + target_x / N;
+              // The next y will be spline(x)
+              double y_pt = s(x_pt);
+              x_addon = x_pt;
+              
+              // Transform X and Y back to global coordinates
+              double x_rel = x_pt;
+              double y_rel = y_pt;
+              x_pt = (x_rel * cos(ref_yaw) - y_rel * sin(ref_yaw));
+              y_pt = (x_rel * sin(ref_yaw) + y_rel * cos(ref_yaw));
+              
+              x_pt += ref_x;
+              y_pt += ref_y;
+              
+              next_x_vals.push_back(x_pt);
+              next_y_vals.push_back(y_pt);
+            }
+            
           	msgJson["next_x"] = next_x_vals;
           	msgJson["next_y"] = next_y_vals;
 
           	auto msg = "42[\"control\","+ msgJson.dump()+"]";
-
+            
           	//this_thread::sleep_for(chrono::milliseconds(1000));
           	ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
           
